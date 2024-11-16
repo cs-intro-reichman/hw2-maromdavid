013bafab983eb10716b092722c726d7d021c5ecf
MAROM DAVID <marom.david@post.runi.ac.il>
2024-11-16 12:45:26 +0200

    second

--- CalcPi.java
+++ CalcPi.java
@@ -2,19 +2,18 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 		int numberOfRounds = Integer.parseInt(args[0]);
-		double odd = 3.0;
-		double sum = 1;
-	    for(int i = 0; i < numberOfRounds; i++)
+		double sum = 0.0;
+	    for(double i = 0; i < numberOfRounds; i++)
                 {
-					sum = sum - (1/odd);
-					//System.out.println(1/odd);
-					odd = odd + 2;
-					sum = sum + (1/odd);
-					//System.out.println(1/odd);
-					odd = odd + 2;
-					i ++;
+					if (i % 2 ==0) {
+						sum += (1/(2 * i + 1));
+					}
+					else{
+						sum -= (1/(2 * i + 1));						
+					}
 				}
+				sum *= 4;
 				System.out.println("pi according to Java: " +Math.PI);
-				System.out.println("pi approximated: " +(Math.PI+(Math.PI/4)-sum));		
+				System.out.println("pi approximated: " +sum);	
 	}
 }
